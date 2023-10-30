7db33df3f52208775297db2e24d8c24eee7a268d
Tooga Garanja Magar <tugamagar77@gmail.com>
2023-10-29 21:32:04 -0400

    Initial Commit

---
+++ Team5_MAPD714_Project-Milestone2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Team5_MAPD714_Project-Milestone2
+//
+//  Created by Tooga Garanja Magar on 2023-10-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
