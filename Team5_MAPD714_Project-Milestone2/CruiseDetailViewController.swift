c7f5a2ffefd121a76ddc854fef263c9ac21325c3
Tooga Garanja Magar <tugamagar77@gmail.com>
2023-10-30 00:16:24 -0400

    Completed Storyboard UI

---
+++ Team5_MAPD714_Project-Milestone2/CruiseDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CruiseDetailViewController.swift
+//  Team5_MAPD714_Project-Milestone2
+//
+//  Created by Tooga Garanja Magar on 2023-10-29.
+//
+
+import UIKit
+
+class CruiseDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Team5_MAPD714_Project-Milestone2/SearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewController.swift
+//  Team5_MAPD714_Project-Milestone2
+//
+//  Created by Tooga Garanja Magar on 2023-10-29.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
